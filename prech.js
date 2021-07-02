d8834e4252ecfc599baec861631c90df13913e0a
omezi precious <preshomezi@gmail.com>
2021-07-01 18:10:33 -0700

    mysites
    furniture

---
+++ prech.js
@@ -0,0 +1,7 @@
+ //function myfucntion() {
+    document.getElementById("demo").innerHTML="hhhhhhgjontrhmhjo4tnh'wtuh'nmhjrwthnrjth'nrthjrwhmwjryh.";
+}
+ 
+function myfucntion() {
+    document.getElementById("demo").innerHTML="dropdown";
+}
